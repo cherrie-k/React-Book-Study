6d674f86d2fdea2fb58696415c348ff97108c8ca
Cherrie <cherriekim925@gmail.com>
2023-01-25 18:16:03 +0900

    changed class components to functional components

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,13 +1,55 @@
 // EventPractice.js
 
-import React, { Component } from 'react';
+// 클래스형 컴포넌트들을 주석 처리하고, 함수 컴포넌트로 재작성해봄
 
+import React, { Component, useState } from 'react';
+/*
 class EventPractice extends Component {
   state = {
     username: '',
     message: '',
   };
+*/
 
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setUsername('');
+    setMessage('');
+  };
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        onChange={onChangeMessage}
+        value={message}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+/*
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -27,7 +69,8 @@ class EventPractice extends Component {
       this.handleClick();
     }
   };
-
+*/
+/*
   render() {
     return (
       <div>
@@ -52,5 +95,6 @@ class EventPractice extends Component {
     );
   }
 }
+*/
 
 export default EventPractice;
