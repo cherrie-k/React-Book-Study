85294937401aa9f595d76abacdfe9f2d2283f2d8
Cherrie <cherriekim925@gmail.com>
2023-02-24 05:23:23 +0900

    리스트 데이터 처리 예시

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,6 +5,7 @@ import EventPractice from './EventPractice';
 import Counter from './Counter';
 import MyComponent from './MyComponent';
 import Input2 from './outsides/Input';
+import UserList from './outsides/List';
 
 const App = () => {
   return (
@@ -13,13 +14,14 @@ const App = () => {
         <Link to="/"> MyComponent </Link> |
         <Link to="/eventpractice"> Event Prtc </Link> |
         <Link to="/counter"> Counter </Link> | <Link to="/input"> Input </Link>{' '}
-        |
+        |<Link to="/list"> List </Link> |
       </nav>
       <Routes>
         <Route path="/" element={<MyComponent />} />
         <Route path="/eventpractice" element={<EventPractice />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/input" element={<Input2 />} />
+        <Route path="/list" element={<UserList />} />
       </Routes>
     </div>
   );
