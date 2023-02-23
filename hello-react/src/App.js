feaca6484ffebfe90514951bf70cfc31283b39e5
Cherrie <cherriekim925@gmail.com>
2023-02-23 22:19:29 +0900

    공부하는 컴포넌트들 router 처리, 폴더 구조 변경

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
 import EventPractice from './EventPractice';
+import Counter2 from './Counter';
+import MyComponent from './MyComponent';
 
 const App = () => {
-  return <EventPractice />;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/"> MyComponent </Link> |
+        <Link to="/eventpractice"> Event Prtc </Link> |
+        <Link to="/counter"> Counter </Link> |
+      </nav>
+      <Routes>
+        <Route path="/" element={<MyComponent />} />
+        <Route path="/eventpractice" element={<EventPractice />} />
+        <Route path="/counter" element={<Counter2 />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
