e11f8f8cba77fd6cf91ec3e3e8adb9e49bf1ccdb
Cherrie <cherriekim925@gmail.com>
2023-02-23 22:20:13 +0900

    간이 카운터 생성

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,5 +1,33 @@
 import { Component } from 'react';
 
+// 간이 카운터
+import React, { useState } from 'react';
+
+const Counter2 = () => {
+  const [num, setNumber] = useState(0);
+
+  const increase = () => {
+    setNumber(num + 1);
+  };
+
+  const decrease = () => {
+    setNumber(num - 1);
+  };
+
+  const divideByTwo = () => {
+    setNumber(num / 1);
+  };
+
+  return (
+    <div>
+      <button onClick={increase}>+1</button>
+      <button onClick={decrease}>-1</button>
+      <button onClick={divideByTwo}>나누기2</button>
+      <p>{num}</p>
+    </div>
+  );
+};
+
 class Counter extends Component {
   state = {
     number: 0,
@@ -31,4 +59,4 @@ class Counter extends Component {
   }
 }
 
-export default Counter;
+export default Counter2;
