c118200dbe3b134955953b2e3a54fe7ce09b698e
Cherrie <cherriekim925@gmail.com>
2023-02-26 23:11:41 +0900

    2장 복습 주석 추가

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,8 +7,11 @@ import MyComponent from './MyComponent';
 import Input2 from './outsides/Input';
 import UserList from './outsides/List';
 
+// JSX는 자바스크립트의 확장 문법. 코드 번들링 과정에서 '바벨'을 사용해서 일반 js 형태의 코드로 변환.
+// 복수의 JSX 컴포넌트는 반드시 부모 요소 하나로 감싸져 있어야 함. 컴포넌트 내부는 하나의 DOM 트리 구조로 이루어져야 한다는 규칙이 있기 때문.
 const App = () => {
   return (
+    // JSX에선 class 대신 className
     <div className="App">
       <nav>
         <Link to="/"> MyComponent </Link> |
@@ -20,7 +23,7 @@ const App = () => {
         <Route path="/" element={<MyComponent />} />
         <Route path="/eventpractice" element={<EventPractice />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="/input" element={<Input2 />} />
+        <Route path="/input" element={<Input2 /> /*'self-closing tag'*/} />
         <Route path="/list" element={<UserList />} />
       </Routes>
     </div>
@@ -28,3 +31,8 @@ const App = () => {
 };
 
 export default App;
+
+/*참고: 
+- JSX 내부에선 if문 대신 {삼항 연산자} 사용
+- 리액트 컴포넌트 함수에서 undefined만 반환한다면 렌더시 오류 발생, but JSX 내부에서 렌더링하는건 가능
+*/
