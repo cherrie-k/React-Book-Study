4fea798e172181f139596a5927cffffc4fdd3a16
Cherrie <cherriekim925@gmail.com>
2023-03-07 18:32:41 +0900

    9장) CSS Module 사용한 클래스 이름 두 개 이상 적용

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -10,7 +10,11 @@ import styles from "./CSSModule.module.css";
 
 const CSSModule = () => {
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={`${styles.wrapper} ${styles.inverted}`}
+      /* 위 백틱(`) 안의 템플릿 리터럴 문법 사용하고 싶지 않다면
+      className = {[styles.wrapper, styles.inverted].join(' ')} */
+    >
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
